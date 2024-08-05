d4ad2bafa7cd3caa7c3a72f6839473c9b69cb3cb
AGV <agonzalez.venegas@outlook.com>
2024-08-05 16:15:31 +0200

    Color.Transparent de cada Card

--- app/src/main/java/com/example/myjsonparsing/MainActivity.kt
+++ app/src/main/java/com/example/myjsonparsing/MainActivity.kt
@@ -16,11 +16,13 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -63,7 +65,7 @@ fun PrintBooks(library: List<BookItem>, modifier: Modifier) {
             .padding(top = 28.dp)
     )
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 150.dp),
+        columns = GridCells.Adaptive(minSize = 140.dp),
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 60.dp)
@@ -77,8 +79,10 @@ fun PrintBooks(library: List<BookItem>, modifier: Modifier) {
 @Composable
 fun BookCard(bookRes: BookItem) {
     Card (
-        //shape = RoundedCornerShape(8.dp),
-        modifier = Modifier.padding(4.dp)
+        modifier = Modifier.padding(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
+        )
     ) {
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
