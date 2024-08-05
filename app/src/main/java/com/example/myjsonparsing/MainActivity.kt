7c7ff1e0ba399bc5615b09dc4da38632b750ab22
AGV <agonzalez.venegas@outlook.com>
2024-08-05 15:00:56 +0200

    LazyVerticalGrid para imagenes y MaterialTheme para titulo

--- app/src/main/java/com/example/myjsonparsing/MainActivity.kt
+++ app/src/main/java/com/example/myjsonparsing/MainActivity.kt
@@ -5,10 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -48,28 +56,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PrintBooks(library: List<BookItem>, modifier: Modifier) {
+    Text(
+        text = "${library.size} libros disponibles",
+        style = MaterialTheme.typography.headlineLarge,
+        modifier = Modifier
+            .padding(top = 28.dp)
+    )
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 150.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp)
+    ) {
+        items(library.size) { index ->
+            BookCard(bookRes = library[index])
+        }
+    }
+}
 
-    LazyColumn {
-        item{
-            Text(
-                text = "TOTAL ELEMENTOS ${library.size}",
-                modifier = Modifier.padding(top = 28.dp)
-            )
-        }
-        items(library) { libro ->
-            Text(
-                text = libro.book.title,
-                modifier = Modifier.padding(top = 4.dp)
-            )
-            // TODO: escalar las imagenes para que todas tengan el mismo tamaño
-            AsyncImage(
-                model = ImageRequest.Builder(context = LocalContext.current)
-                    .data(libro.book.cover)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = null,
-                modifier = Modifier.padding(start = 8.dp)
-            )
-        }
+@Composable
+fun BookCard(bookRes: BookItem) {
+    Card (
+        //shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(4.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(bookRes.book.cover)
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = 2.dp)
+                .fillMaxWidth()
+                .aspectRatio(1.0f)
+        )
+
     }
 }
