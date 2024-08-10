65a31207400fe65544d4f9b24b25982796f41dd0
AGV <agonzalez.venegas@outlook.com>
2024-08-10 17:22:12 +0200

    Filtro generos centrado y con boton cuqui

--- app/src/main/java/com/example/myjsonparsing/MainActivity.kt
+++ app/src/main/java/com/example/myjsonparsing/MainActivity.kt
@@ -15,10 +15,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -32,6 +37,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -99,7 +105,8 @@ fun PrintBooks(library: List<BookItem>, modifier: Modifier) {
                     Text(
                         text = "Filtrar por páginas",
                         modifier = Modifier
-                            .fillMaxWidth(),
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
                         textAlign = TextAlign.Center
                     )/*
                     BookFilter(
@@ -117,8 +124,9 @@ fun PrintBooks(library: List<BookItem>, modifier: Modifier) {
                     Text(
                         text = "Filtrar por género",
                         modifier = Modifier
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.Center
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
+                        textAlign = TextAlign.Center,
                     )
                     BookFilter(
                         current = selectedGenre,
@@ -187,7 +195,20 @@ fun BookFilter(
             .clickable { showDropdown = true }
             .padding(8.dp)
     ) {
-        Text(text = current)
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            IconButton(
+                onClick = {showDropdown = true}
+            ) {
+                Icon(
+                    Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Filter by genre"
+                )
+            }
+            Text(text = current)
+        }
+
 
         DropdownMenu(
             expanded = showDropdown,
