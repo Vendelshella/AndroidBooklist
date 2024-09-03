60a4f67c71af255db9aa2fda33ab883714623cc1
Metalex84 <agonzalez.venegas@outlook.com>
2024-09-03 18:15:02 +0200

    Application aislada

---
+++ app/src/main/java/com/example/androidbooklist/data/LibraryApp.kt
@@ -0,0 +1,17 @@
+package com.example.androidbooklist.data
+
+import android.app.Application
+import androidx.room.Room
+
+class LibraryApp : Application() {
+
+    lateinit var db: LibraryDatabase
+
+    override fun onCreate() {
+        super.onCreate()
+        db = Room
+            .databaseBuilder(this, LibraryDatabase::class.java, "library_database")
+            .build()
+    }
+
+}
